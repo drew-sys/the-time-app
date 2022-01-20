18bbef9cc0f1da4a5751aba203c4fdccc8692072
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-01-20 14:26:46 +0000

    Update time_app.py

--- time_app.py
+++ time_app.py
@@ -87,8 +87,8 @@ with col2:
     )
 
 help_text = '''
-##### \* 
-_A meeting block is a series of back-to-back meetings separated by no more than 5 minutes. You should have less meeting blocks than meetings._
+#######
+\* _A meeting block is a series of back-to-back meetings separated by no more than 5 minutes. You should have less meeting blocks than meetings._
 \n _Context switching refers to time it takes you to get back into a productive working mode. The average cost of context switching is 22 minutes and varies depending on the type of task you are undertaking._
 '''
 st.markdown(help_text)
